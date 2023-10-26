b2c9ca6b34860cc003809047cc761e4f1829b2cf
drevkr <hadree.mustaffa@gmail.com>
2023-10-26 21:57:48 +0800

    add function to close all active submenu

--- index.js
+++ index.js
@@ -20,29 +20,57 @@ const toggleMainMenu = () => {
   mainMenu.classList.toggle('open');
 };
 
-const expandSubmenu = (itemSelector) => {
-  const isExpanded = itemSelector.getAttribute('aria-expanded') === 'true';
-  const itemArrow = itemSelector.querySelector('.arrow');
-  const itemList = itemSelector.nextElementSibling;
-
-  if (!isExpanded) {
-    itemSelector.setAttribute('aria-expanded', 'true');
-    itemArrow.style.transform = 'rotate(180deg)';
-    itemList.style.display = 'flex';
-  } else {
-    itemSelector.setAttribute('aria-expanded', 'false');
-    itemArrow.style.transform = 'rotate(0)';
-    itemList.style.display = 'none';
-  }
-  itemSelector.classList.toggle('active');
+const expandSubmenu = (itemSelector, listIcon) => {
+  itemSelector.setAttribute('aria-expanded', 'true');
+  listIcon.style.transform = 'rotate(180deg)';
+  itemSelector.classList.add('active');
 };
 
+const collapseSubmenu = (itemSelector, listIcon) => {
+  itemSelector.setAttribute('aria-expanded', 'false');
+  listIcon.style.transform = 'rotate(0)';
+  itemSelector.classList.remove('active');
+};
+
+const collapseActiveSubmenu = () => {
+  dropdownMenu.forEach((item) => {
+    collapseSubmenu(item, item.querySelector('.arrow'));
+  });
+};
+
+// event listeners
 btnToggle.addEventListener('click', () => {
   toggleMainMenu();
 });
 
-Array.from(dropdownMenu).forEach((item) => {
-  item.addEventListener('click', () => {
-    expandSubmenu(item);
+dropdownMenu.forEach((item) => {
+  const itemArrow = item.querySelector('.arrow');
+  const itemList = item.nextElementSibling;
+
+  item.addEventListener('click', (e) => {
+    if (item.getAttribute('aria-expanded') === 'false') {
+      expandSubmenu(item, itemArrow);
+    } else {
+      collapseSubmenu(item, itemArrow);
+    }
   });
+
+  itemList.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      collapseSubmenu(item, itemArrow);
+      item.focus({ focusVisible: true });
+    }
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    collapseActiveSubmenu();
+  }
+});
+
+window.addEventListener('click', (e) => {
+  if (!e.target.matches('.dropdown')) {
+    collapseActiveSubmenu();
+  }
 });
